b74d83511ada8665de2b4b97fbbe911688120896
Bruce Ediger <bediger@stratigery.com>
2016-10-13 15:46:02 -0600

    Moved cb.c code and cb.h headers and definitions into ac7.c to make it have less complex file structure.

--- ac7.c
+++ ac7.c
@@ -5,7 +5,32 @@
 #include <ctype.h>
 
 #include <ac7.h>
-#include <cb.h>
+
+/* Queue-as-circular-buffer data structure. */
+#define CBUFSIZE 0x200  /* 512 */
+#define CBUFMASK 0x1ff  /* 512 - 1 */
+
+struct queue_node {
+	int state;
+	struct queue_node *next;
+};
+
+struct queue {
+	int *cbuf;
+	int  in;
+	int  out;
+	struct queue_node *ovfhead;
+	struct queue_node *ovftail;
+};
+
+
+struct queue *queueinit(void);
+void queuedestroy(struct queue *);
+void enqueue(struct queue *, int);
+int  dequeue(struct queue *);
+int  queueempty(struct queue *);
+void empty_error(struct queue *);
+
 
 struct gto *
 init_goto()
@@ -329,4 +354,113 @@ construct_delta(struct gto *g)
 	}
 
 	queuedestroy(q);
+}
+
+struct queue *
+queueinit()
+{
+	struct queue *r;
+
+	r = (struct queue *)malloc(sizeof(*r));
+	assert(NULL != r);
+
+	r->cbuf = (int *)malloc(CBUFSIZE*sizeof(int));
+	assert(NULL != r->cbuf);
+
+	r->in = 0;
+	r->out = 0;
+	r->ovfhead = r->ovftail = NULL;
+
+	return r;
+}
+
+void
+empty_error(struct queue *q)
+{
+	fprintf(stderr, "Tried to get off empty queue: in %d, out %d\n", q->in, q->out);
+	exit(9);
+}
+
+void
+enqueue(struct queue *q, int state)
+{
+	struct queue_node *newnode;
+
+	assert(NULL != q);
+
+	if (((q->in + 1)&CBUFMASK) != q->out)
+	{
+		assert(NULL != q->cbuf);
+		q->cbuf[q->in] = state;
+		q->in = (q->in+1)&CBUFMASK;
+
+	} else {
+
+		newnode = (struct queue_node *)malloc(sizeof *newnode);
+		assert(NULL != newnode);
+		newnode->state = state;
+		newnode->next = NULL;
+
+		if (NULL == q->ovftail)
+		{
+			q->ovfhead = q->ovftail = newnode;
+		} else {
+			q->ovftail->next = newnode;
+			q->ovftail = newnode;
+		}
+	}
+}
+
+int
+queueempty(struct queue *q)
+{
+	assert(NULL != q);
+	return (q->in == q->out);
+}
+
+int
+dequeue(struct queue *q)
+{
+	int r;
+
+	assert(NULL != q);
+	if (q->in == q->out) empty_error(q);
+
+	assert(NULL != q->cbuf);
+	r = q->cbuf[q->out];
+
+	q->out = (q->out + 1)&CBUFMASK;
+
+	if (NULL != q->ovfhead)
+	{
+		struct queue_node *tmp;
+
+		q->cbuf[q->in]= q->ovfhead->state;
+		q->in = (q->in+1)&CBUFMASK;
+		tmp = q->ovfhead;
+		q->ovfhead = q->ovfhead->next;
+		if (NULL == q->ovfhead) q->ovftail = NULL;
+		free(tmp);
+	}
+
+	return r;
+}
+
+void
+queuedestroy(struct queue *q)
+{
+	assert(NULL != q);
+
+	if (NULL != q->cbuf) free(q->cbuf);
+
+	while (NULL != q->ovfhead)
+	{
+		struct queue_node *t = q->ovfhead->next;
+
+		free(q->ovfhead);
+
+		q->ovfhead = t;
+	}
+
+	free(q);
 }
--- makefile
+++ makefile
@@ -1,12 +1,11 @@
 CC = gcc
 CFLAGS = -I. -g -Wall -Wextra #-fsanitize=undefined -fsanitize=address
 
-ac: ac_main.o ac7.o cb.o
-	$(CC) $(CFLAGS) -o ac ac_main.o ac7.o cb.o
+ac: ac_main.o ac7.o
+	$(CC) $(CFLAGS) -o ac ac_main.o ac7.o
 
-cb.o: cb.c cb.h
 
-ac7.o: ac7.c ac7.h cb.h
+ac7.o: ac7.c ac7.h
 
 ac_main.o: ac_main.c ac7.h
 

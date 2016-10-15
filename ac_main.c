967e1742a285514fabebce12f24332f1d3c33aee
Bruce Ediger <bediger@stratigery.com>
2016-10-15 10:12:58 -0600

    Simplify the keyword array creation. Gee whiz.

--- ac_main.c
+++ ac_main.c
@@ -7,26 +7,13 @@
 int
 main(int ac, char **av)
 {
-	int i, k = 0;
-	char **keywords;
 	struct gto *g;
 	char bp[BUFSIZ];
 
-	keywords = (char **)malloc(sizeof(char *) * (ac - 1));
+	if (1 == ac)
+		return 1;
 
-	for (i = 1; i < ac; ++i)
-	{
-		keywords[i - 1] = malloc(strlen(av[i]) + 1);
-
-		strcpy(keywords[i-1], av[i]);
-		++k;
-	}
-
-	g = init_goto();
-
-	construct_goto(keywords, k, g);
-	construct_failure(g);
-	construct_delta(g);
+	g = initialize_matching(ac - 1, &av[1]);
 
 	/* do the matching */
 	while (NULL != (fgets(bp, sizeof(bp), stdin)))
@@ -37,11 +24,6 @@ main(int ac, char **av)
 	}
 
 	destroy_goto(g);
-
-	for (i = 0; i < ac - 1; ++i)
-		free(keywords[i]);
-
-	free(keywords);
 
 	return 0;
 }
